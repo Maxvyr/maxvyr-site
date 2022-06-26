41724ce351fdfd5375da80653578753c7ac5cd33
maxvyr <maxvid30@gmail.com>
2022-06-26 22:10:32 +0200

    💄 add topbar to all screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Footer from '../components/footer'
 import TopBar from '../components/top_bar'
 import styles from '../styles/Home.module.css'
--- pages/screen/contact.tsx
+++ pages/screen/contact.tsx
@@ -1,17 +1,25 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import Footer from '../../components/footer'
+import TopBar from '../../components/top_bar'
+import styles from '../../styles/contact.module.css'
 
 const Contact : NextPage = () => {
     return (
         <>
-            <h1>Contact Screen</h1>
-            <h2>
-                <Link href="/">
-                <a>Back to home</a>
-                </Link>
-            </h2>
-            <Footer />
+            <main className={styles.main}>
+                <TopBar />
+                <h1>Contact Screen</h1>
+                <h2>
+                    <Link href="/">
+                    <a>Back to home</a>
+                    </Link>
+                </h2> 
+            </main>
+
+            <footer className={styles.footer}>
+                <Footer />
+            </footer>
         </>
     )
 }
