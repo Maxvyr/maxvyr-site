88a5c8e8b3fbf15cc1885149322c3c46328a5372
maxvyr <maxvid30@gmail.com>
2022-07-22 11:13:37 +0200

    ✨ add newsletter link

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,13 +2,27 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className='container flex justify-between items-center flex-col py-[55px] sm:flex-row'>
+    <footer className="container flex justify-between items-center flex-col py-[55px] sm:flex-row">
       <p>&copy; Site web personnel</p>
-
-      <p>Merci spécial à <a target="_blank" href="https://icons8.com" className='font-bold'>Icons8</a> ❤️</p>
+      <a
+        href="http://eepurl.com/hJKBFj"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        S'incrire a la newsletters
+      </a>
 
       <p>
-        Site réalisé avec <span className='font-bold'>Next JS, Tailwind</span> ❤️
+        Merci spécial à{" "}
+        <a target="_blank" href="https://icons8.com" className="font-bold">
+          Icons8
+        </a>{" "}
+        ❤️
+      </p>
+
+      <p>
+        Site réalisé avec <span className="font-bold">Next JS, Tailwind</span>{" "}
+        ❤️
       </p>
     </footer>
   );
