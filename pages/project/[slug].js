17beb5756462140e986c787486741113efc9db6a
maxvyr <maxvid30@gmail.com>
2022-08-05 15:33:54 +0200

    ✨ update color button on each project

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -30,7 +30,7 @@ export default function project({ work }) {
           </div>
           <div className="flex lg:justify-around  gap-3 my-10">
             <a
-              className="px-[50px] py-[10px] border-2 rounded-lg border-white hover:border-gray hover:scale-105 hover:bg-white hover:text-black"
+              className="px-[50px] py-[10px] border-2 rounded-lg text-black border-black hover:border-gray hover:scale-105 hover:bg-white hover:text-black"
               href={`${work?.link_code_base}`}
               target="_blank"
               rel="noreferrer"
@@ -38,7 +38,7 @@ export default function project({ work }) {
               Voir le code
             </a>
             <a
-              className="px-[50px] py-[10px] border-2 rounded-lg border-white hover:border-gray hover:scale-105 hover:bg-white hover:text-black"
+              className="px-[50px] py-[10px] border-2 rounded-lg text-black border-black hover:border-gray hover:scale-105 hover:bg-white hover:text-black"
               href={`${work?.link_project}`}
               target="_blank"
               rel="noreferrer"
